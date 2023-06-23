a1185100c2e719e9ba9b669c6151d78fa19d0db2
Sekod <101645781+Sekod@users.noreply.github.com>
2023-06-22 22:04:18 -0400

    cambios style y login

--- appCourier/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import Solicitud
-class SolicitudForm(forms.ModelForm):
-    class Meta:
-        model = Solicitud
-        fields = ['codigo', 'nombre','direccion_origen', 'direccion_destino','estado']
--- appCourier/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from appCourier.models import Solicitud
-
-class SolicitudSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Solicitud
-        fields = '__all__'
-        
